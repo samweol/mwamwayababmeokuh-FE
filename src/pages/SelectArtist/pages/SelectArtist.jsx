208fdacdb9165c8fcb4bca6c0c938a3de8db22b1
samweol <sejuyes@gmail.com>
2023-12-27 16:18:34 +0900

    STYLE: 레이아웃 컨텐트 영역 분리

--- src/pages/SelectArtist/pages/SelectArtist.jsx
+++ src/pages/SelectArtist/pages/SelectArtist.jsx
@@ -3,20 +3,32 @@ import Layout from "../../../components/Layout/Layout";
 import SearchBar from "../../../components/SearchBar/SearchBar";
 import Button from "../../../components/Button/Button";
 import NoneArtist from "../components/NoneArtist/NoneArtist";
+import useNavigatePage from "../../../hooks/useNavigatePage";
+import LayoutContent from "../../../components/Layout/LayoutContent";
 
 export default function SelectArtis() {
   const [keyword, setKeyword] = useState("");
   const [artists, setArtists] = useState([]);
+  const { navigatePage } = useNavigatePage();
   return (
     <Layout>
-      <SearchBar
-        keyword={keyword}
-        onChangeHandler={(e) => {
-          setKeyword(e.target.value);
-        }}
-      />
-      {artists.length ? null : <NoneArtist />}
-      <Button bottomFixed={true}> Skip</Button>
+      <LayoutContent>
+        <SearchBar
+          keyword={keyword}
+          onChangeHandler={(e) => {
+            setKeyword(e.target.value);
+          }}
+        />
+        {artists.length ? null : <NoneArtist />}
+        <Button
+          bottomFixed={true}
+          onClickHandler={() => {
+            navigatePage("/");
+          }}
+        >
+          Skip
+        </Button>
+      </LayoutContent>
     </Layout>
   );
 }
