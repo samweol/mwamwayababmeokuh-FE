8384de8bc9580ffce069088140a796b4d889ab87
samweol <sejuyes@gmail.com>
2024-01-09 15:25:49 +0900

    FEAT: 첫 렌더링시 Home으로 잠시 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
 import BottomTab from "./components/BottomTab/BottomTab";
 import Layout from "./components/Layout/Layout";
 import Navbar from "./components/Navbar/Navbar";
+import useNavigatePage from "./hooks/useNavigatePage";
 
 function App() {
+  const { navigatePage } = useNavigatePage();
+  useEffect(() => {
+    navigatePage("/home");
+  }, []);
   return (
     <Layout>
       <Navbar />
