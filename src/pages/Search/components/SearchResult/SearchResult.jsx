4dd9b7e16ed7f58d4bc00f505afc01a9aba9e103
samweol <sejuyes@gmail.com>
2024-02-15 16:23:18 +0900

    FEAT: 검색 api 연결

--- src/pages/Search/components/SearchResult/SearchResult.jsx
+++ src/pages/Search/components/SearchResult/SearchResult.jsx
@@ -1,5 +1,9 @@
 import Post from "../../../../components/Post/Post";
 
-export default function SearchResult() {
-  return <section>{/* <Post /> */}</section>;
+export default function SearchResult(props) {
+  const { searchResultList } = props;
+  const postList = searchResultList?.map((item) => (
+    <Post key={item.pid} post={item} />
+  ));
+  return <section>{postList}</section>;
 }
--- src/pages/Search/pages/Search.jsx
+++ src/pages/Search/pages/Search.jsx
@@ -3,21 +3,61 @@ import LayoutContent from "../../../components/Layout/LayoutContent";
 import BottomTab from "../../../components/BottomTab/BottomTab";
 import SearchHeader from "../../../components/Header/SearchHeader";
 import Ranking from "../components/Ranking/Ranking";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchResult from "../components/SearchResult/SearchResult";
 import useDebounce from "../../../hooks/useDebounce";
+import { api } from "../../../api/baseURL";
+import { useSetRecoilState } from "recoil";
+import { loadingState } from "../../../recoil/atom";
+import Loading from "../../../components/Loading/Loading";
 
 export default function Search() {
   const [keyword, setKeyword] = useState("");
   const { debounceValue } = useDebounce(keyword, 3000);
 
+  const [artistPostList, setArtistPostList] = useState([]);
+  const [hashtagPostList, setHashtagPostList] = useState([]);
+
+  const setIsLoading = useSetRecoilState(loadingState);
+
   const onSelectRankKeyword = (keyword) => {
     setKeyword(keyword);
   };
 
-  const searchFun = () => {
-    console.log(debounceValue);
+  console.log("debounceValue : ", debounceValue);
+  console.log(artistPostList);
+
+  /**
+   * 검색 api
+   */
+  const searchKeyword = async () => {
+    console.log("searchKeyword function");
+    console.log(`/boards/search-results?searchKeyword=${debounceValue}`);
+    try {
+      setIsLoading(true);
+      const resp = await api.get(
+        `/boards/search-results?searchKeyword=${debounceValue}`
+      );
+
+      console.log("결과");
+      console.log(resp.data);
+      setArtistPostList(resp.data.byArtist);
+      setHashtagPostList(resp.data.byHashtag);
+
+      console.log("🌟검색 성공🌟");
+    } catch (err) {
+      console.error(err);
+      console.log("🔥검색 실패🔥");
+    } finally {
+      setIsLoading(false);
+    }
   };
+
+  useEffect(() => {
+    if (debounceValue.length) {
+      searchKeyword();
+    }
+  }, [debounceValue]);
 
   return (
     <Layout>
@@ -28,16 +68,19 @@ export default function Search() {
         }}
         onBackBtnHandler={() => {
           setKeyword("");
+          setArtistPostList([]);
+          setHashtagPostList([]);
         }}
       />
       <LayoutContent padding={true}>
         {keyword ? (
-          <SearchResult />
+          <SearchResult searchResultList={artistPostList} />
         ) : (
           <Ranking onClickHandler={onSelectRankKeyword} />
         )}
       </LayoutContent>
       <BottomTab />
+      <Loading />
     </Layout>
   );
 }
