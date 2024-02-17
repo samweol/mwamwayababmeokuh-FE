0843e22ef3728e0eddcc6047c9e68dad5d824a36
samweol <sejuyes@gmail.com>
2024-02-17 20:52:46 +0900

    STYLE: 파비콘, 메인 로고 변경

--- src/components/Header/MainHeader.jsx
+++ src/components/Header/MainHeader.jsx
@@ -1,8 +1,15 @@
 import styles from "./MainHeader.module.scss";
+import Main from "../../assets/logo/main.png";
 export default function MainHeader() {
   return (
     <header className={styles["mainheader-cont"]}>
-      <h1 className={styles["mainheader-logo"]}>메인로고</h1>
+      <h1 className={styles["mainheader-logo"]}>
+        <img
+          className={styles.mainheaderLogo}
+          src={Main}
+          alt="메인로고 아이콘"
+        />
+      </h1>
     </header>
   );
 }
