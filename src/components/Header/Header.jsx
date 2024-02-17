c7d0ce0726ba1407ef63b3bad2dc8ead100f3f60
samweol <sejuyes@gmail.com>
2024-02-17 17:03:37 +0900

    FEAT: 로그아웃 모달 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,9 @@ import styles from "./Header.module.scss";
 import Button from "../../components/Button/Button";
 import useNavigatePage from "../../hooks/useNavigatePage";
 import BackArrow from "../../assets/images/icon-arrow-left.png";
+import MoreIcon from "../../assets/images/more-vertical.png";
 export default function Header(props) {
-  const { title, buttonText, onClickHandler } = props;
+  const { title, buttonText, moreBtn, onClickHandler } = props;
   const { navigatePage } = useNavigatePage();
   return (
     <header className={styles["basic-header"]}>
@@ -16,11 +17,15 @@ export default function Header(props) {
         <img src={BackArrow} alt="뒤로가기 버튼" />
       </button>
       <h1 className={styles["header-title"]}>{title}</h1>
-      {buttonText && (
+      {buttonText ? (
         <Button size="s" onClickHandler={onClickHandler}>
           {buttonText}
         </Button>
-      )}
+      ) : moreBtn ? (
+        <button onClick={onClickHandler}>
+          <img src={MoreIcon} />
+        </button>
+      ) : null}
     </header>
   );
 }
